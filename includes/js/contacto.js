f8d77b32459c913c7bfbeda0a6ea068133af6f94
Cristian K. Cárdenas <ccardensa@outlook.com>
2019-01-31 16:48:27 -0300

    Add files via upload

---
+++ includes/js/contacto.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    $("#CaptchaInputText").addClass("form-control");
+    $("#form-contact").submit(function (event) {
+        $("#mensaje").empty();
+        $("#mensaje").removeClass("alert alert-success alert-danger");
+        $.ajax(
+              {
+                  url: 'Contacto/Index',
+                  dataType: 'json',
+                  data: $(this).serialize(),
+                  type: 'POST',
+                  success: function (data) {
+                      $(this).empty();
+                      if (data.isOK) {
+                          $("#mensaje").addClass("alert alert-success");
+                          $("#mensaje").append(data.mensaje);
+                          $('#mensaje').fadeIn('fast').delay(15000).fadeOut('slow');
+                      } else {
+
+                          $("#mensaje").addClass("alert alert-danger");
+                          $("#mensaje").append(data.mensaje);
+                          $('#mensaje').fadeIn('fast').delay(4000).fadeOut('slow');
+                      }
+                  },
+                  error: function (xhr) {
+                      //alert(xhr.statusText);
+                  }
+              });
+        event.preventDefault();
+    });
+});
