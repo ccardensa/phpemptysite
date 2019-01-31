43b6d5088231587f662414e272e18b71a335be61
Cristian K. Cárdenas <ccardensa@outlook.com>
2019-01-31 16:52:01 -0300

    Add files via upload

---
+++ includes/kendo/js/common.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    var width = $(window).width(), height = $(window).height();
+    if ((width <= 480)) {
+       //Lo que no se debe cargar en el móvil
+    } else {
+       
+        $('.carousel').carousel({
+            interval: 10000
+        });
+    }
+
+   
+});
